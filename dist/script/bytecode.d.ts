db97b509077b27a6e607024eec3f5634aba53551
Adam Kloboucnik <adam@klob.cz>
2019-10-14 15:02:03 +0200

    Add script/bytecode module and test

---
+++ dist/script/bytecode.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2017-present, BlockCollider developers, All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+/// <reference types="node" />
+export declare const OPS: Map<number, string>;
+export declare const REVERSE_OPS: Map<string, number>;
+export declare const BYTECODE_VERSIONS: Map<number, Buffer>;
+export declare function fromASM(asm: string, version: number): Buffer;
+export declare function toASM(bytecode: Buffer, version: number): string;
