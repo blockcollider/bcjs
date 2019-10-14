71d7ff38b7a0bfce71210c0776e895a386470e99
Adam Kloboucnik <adam@klob.cz>
2019-10-14 16:01:32 +0200

    Port isHexString and normalizeHexString functions

---
+++ src/utils/__tests__/string.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2018-present, BlockCollider developers, All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { isHexString, normalizeHexString } from '../string'
+
+describe('string utils', () => {
+  describe('isHexString', () => {
+    const tests: [string, boolean][] = [
+      ['c0ffee', true],
+      ['C0FFEE', true],
+      ['C0fFeE', true],
+      ['A', true],
+      ['a', true],
+      ['1', true],
+      ['nC0fFeE', false],
+      ['C0fFeEn', false],
+      ['', false],
+      ['0x', false],
+      ['0x1', true],
+      ['cf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701', true],
+      ['0xcf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701', true],
+      ['0Xcf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701', false]
+    ]
+
+    for (const [testData, result] of tests) {
+      it(`recognizes ${JSON.stringify(testData)} as ${result ? 'valid' : 'invalid'} hex string`, () => {
+        expect(isHexString(testData)).toBe(result)
+      })
+    }
+  })
+
+  describe('normalizeHexString', () => {
+    const tests = [
+      ['c0ffee', '0xc0ffee'],
+      ['C0FFEE', '0xc0ffee'],
+      ['C0fFeE', '0xc0ffee'],
+      ['A', '0xa'],
+      ['a', '0xa'],
+      ['1', '0x1'],
+      ['0x1', '0x1'],
+      [
+        'cf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701',
+        '0xcf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701'
+      ],
+      [
+        '0xcf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701',
+        '0xcf8ea96aee4752b794558649acfafc0db1cf6206bdfd8ec2f2de6654893af14d0a7991c4a76f5a55232c9ddd5cb3aeabecc4b5cd69b83680ceacb18dff81972701'
+      ]
+    ]
+
+    for (const [testData, result] of tests) {
+      it(`normalizes ${JSON.stringify(testData)} as ${result}`, () => {
+        expect(normalizeHexString(testData)).toBe(result)
+      })
+    }
+  })
+})
---
+++ src/utils/string.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2017-present, BlockCollider developers, All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+export function isHexString (val: string): boolean {
+  if (val.startsWith('0x')) {
+    val = val.slice(2)
+  }
+  if (!val) {
+    return false
+  }
+  const validHexChars = new Set('0123456789abcdefABCDEF'.split(''))
+  for (let char of val) {
+    if (!validHexChars.has(char)) {
+      return false
+    }
+  }
+  return true
+}
+
+export function normalizeHexString (hexStr: string): string {
+  if (!isHexString(hexStr)) {
+    throw new Error(`${hexStr} is not a valid hex string`)
+  }
+
+  let result = hexStr.toLowerCase()
+
+  if (!result.startsWith('0x')) {
+    result = `0x${result}`
+  }
+
+  return result
+}
