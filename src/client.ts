7235540f22ff930e0165f3a2fbb221f9618b1c19
Adam Kloboucnik <adam@klob.cz>
2019-11-14 17:54:56 +0100

    Fix lints in client module

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,10 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+/* tslint:disable:max-line-length */
+/* tslint:disable:interface-name */
+require('es6-promise').polyfill() /* tslint:disable-line */
+require('isomorphic-fetch') /* tslint:disable-line */
 
-import * as core from './protos/core_pb';
-import * as bc from './protos/bc_pb';
+import * as bc from './protos/bc_pb'
+import * as core from './protos/core_pb'
 
 type BcRpcResponse =
     core.BcBlock.AsObject |
@@ -23,95 +25,94 @@ type BcRpcResponse =
     bc.VanityConvertResponse.AsObject
 
 type JsonRpcParams = Array<string | number> | Buffer
-type JsonRpcVersion = "2.0";
-type JsonRpcReservedMethod = string;
-type JsonRpcId = number | string | void;
+type JsonRpcVersion = '2.0'
+type JsonRpcReservedMethod = string
+type JsonRpcId = number | string | void
 
 enum BcRpcMethod {
     // Help  = "help",
     // Stats  = "stats",
-    NewTx  = "newTx",
 
-    GetBalance  = "getBalance",
-    GetWallet  = "getWallet",
-    GetSpendableCollateral  = "getSpendableCollateral",
+    GetBalance  = 'getBalance',
+    GetWallet  = 'getWallet',
+    GetSpendableCollateral  = 'getSpendableCollateral',
 
-    UnlockCollateral  = "unlockCollateral",
-    GetUnlockTakerTxParams = "getUnlockTakerTxParams",
+    UnlockCollateral  = 'unlockCollateral',
+    GetUnlockTakerTxParams = 'getUnlockTakerTxParams',
 
-    PlaceMakerOrder  = "placeMakerOrder",
-    PlaceTakerOrder  = "placeTakerOrder",
-    PlaceTakerOrders  = "placeTakerOrders",
+    PlaceMakerOrder  = 'placeMakerOrder',
+    PlaceTakerOrder  = 'placeTakerOrder',
+    PlaceTakerOrders  = 'placeTakerOrders',
 
-    CalculateMakerFee = "calculateMakerFee",
-    CalculateTakerFee  = "calculateTakerFee",
+    CalculateMakerFee = 'calculateMakerFee',
+    CalculateTakerFee  = 'calculateTakerFee',
 
-    GetOpenOrders = "getOpenOrders",
-    GetMatchedOrders  = "getMatchedOrders",
-    GetBlake2bl  = "getBlake2bl",
-    GetBcAddressViaVanity  = "getBcAddressViaVanity",
+    GetOpenOrders = 'getOpenOrders',
+    GetMatchedOrders  = 'getMatchedOrders',
+    GetBlake2bl  = 'getBlake2bl',
+    GetBcAddressViaVanity  = 'getBcAddressViaVanity',
 
-    GetLatestBlock = "getLatestBlock",
-    GetLatestRoveredBlocks = "getLatestRoveredBlocks",
-    GetBlockHeight = "getBlockHeight",
-    GetBlockHash = "getBlockHash",
-    GetRoveredBlockHeight = "getRoveredBlockHeight",
-    GetRoveredBlockHash = "getRoveredBlockHash",
-    GetBlocks = "getBlocks",
-    GetRoveredBlocks = "getRoveredBlocks",
-    GetTx = "getTx",
-    GetMarkedTx = "getMarkedTx",
-    SendTx = 'sendTx'
+    GetLatestBlock = 'getLatestBlock',
+    GetLatestRoveredBlocks = 'getLatestRoveredBlocks',
+    GetBlockHeight = 'getBlockHeight',
+    GetBlockHash = 'getBlockHash',
+    GetRoveredBlockHeight = 'getRoveredBlockHeight',
+    GetRoveredBlockHash = 'getRoveredBlockHash',
+    GetBlocks = 'getBlocks',
+    GetRoveredBlocks = 'getRoveredBlocks',
+    GetTx = 'getTx',
+    GetMarkedTx = 'getMarkedTx',
+    SendTx = 'sendTx',
 
 }
 
 interface JsonRpcRequest<T> {
-  jsonrpc: JsonRpcVersion;
-  id: JsonRpcId;
-  method: T;
-  params?: JsonRpcParams;
+  jsonrpc: JsonRpcVersion
+  id: JsonRpcId
+  method: T
+  params?: JsonRpcParams
 }
 
 interface JsonRpcResponse<T> {
-  jsonrpc: JsonRpcVersion;
-  id: JsonRpcId;
+  jsonrpc: JsonRpcVersion
+  id: JsonRpcId
 }
 
 interface JsonRpcSuccess<T> extends JsonRpcResponse<T> {
-    result: T;
+    result: T
 }
 
 interface JsonRpcFailure<T> extends JsonRpcResponse<T> {
-    error: JsonRpcError<T>;
+    error: JsonRpcError<T>
 }
 
 interface JsonRpcError<T> {
       /** Must be an integer */
-      code: number;
-      message: string;
-      data?: T;
+      code: number
+      message: string
+      data?: T
 }
 
-function btoa(str: string | Buffer) {
-    var buffer;
+function btoa (str: string | Buffer) {
+    let buffer
 
     if (str instanceof Buffer) {
-        buffer = str;
+        buffer = str
     } else {
-        buffer = Buffer.from(str.toString(), 'binary');
+        buffer = Buffer.from(str.toString(), 'binary')
     }
 
-    return buffer.toString('base64');
+    return buffer.toString('base64')
 }
 
 export default class RpcClient {
     private rpcUrl: URL
     private defaultHeaders: { [key: string]: string }
 
-    constructor(nodeUrl: string, authToken?: string) {
-        this.rpcUrl = new URL(nodeUrl);
+    constructor (nodeUrl: string, authToken?: string) {
+        this.rpcUrl = new URL(nodeUrl)
         if (authToken) {
-            this.defaultHeaders = { 'Content-Type': 'application/json', authorization: 'Basic ' + btoa(`:${authToken}`) }
+            this.defaultHeaders = { 'Content-Type': 'application/json', 'authorization': 'Basic ' + btoa(`:${authToken}`) }
         } else {
             if (this.rpcUrl.protocol === 'https:') {
                 throw new Error('You have to provide an authToken with https:// scheme')
@@ -120,98 +121,52 @@ export default class RpcClient {
         }
     }
 
-    private async makeJsonRpcRequest(method: BcRpcMethod, rpcParams: JsonRpcParams|Buffer): Promise<BcRpcResponse|JsonRpcError<BcRpcResponse>> {
-        const id = Math.abs(Math.random() * 1e6 | 0)
-        const rpcBody: JsonRpcRequest<BcRpcMethod> = {
-            id,
-            jsonrpc: '2.0',
-            method,
-            params: rpcParams
-        }
-
-        let res
-        try {
-            res = await fetch(`${this.rpcUrl.origin}/rpc`, {
-                method: 'post',
-                body: JSON.stringify(rpcBody),
-                headers: this.defaultHeaders
-            })
-        } catch (e) {
-            return {
-                code: -1,
-                message: e.toString()
-            }
-        }
-
-        if (res.status !== 200) {
-            return {
-                code: res.status,
-                message: res.statusText
-            }
-        }
-
-        let jsonResult: JsonRpcResponse<BcRpcResponse> = await res.json()
-
-        if ("code" in jsonResult) {
-            return jsonResult
-        }
-
-        if (jsonResult.id !== id) {
-            return {
-                message: `Ids didn't match - sent ${id}, got ${jsonResult.id}`,
-                code: -32603,
-            }
-        }
-
-        return (jsonResult as JsonRpcSuccess<BcRpcResponse>).result
-    }
-
-    public async getRoveredBlockHash(request: bc.GetRoveredBlockHashRequest): Promise<core.Block.AsObject|Error> {
+    public async getRoveredBlockHash (request: bc.GetRoveredBlockHashRequest): Promise<core.Block.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetRoveredBlockHash, request.toArray())
         return result as core.Block.AsObject
     }
 
-    public async getRoveredBlockHeight(request: bc.GetRoveredBlockHeightRequest): Promise<core.Block.AsObject|Error> {
+    public async getRoveredBlockHeight (request: bc.GetRoveredBlockHeightRequest): Promise<core.Block.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetRoveredBlockHeight, request.toArray())
         return result as core.Block.AsObject
     }
 
-    public async getRoveredBlocks(request: bc.GetRoveredBlocksRequest): Promise<bc.GetRoveredBlocksResponse.AsObject|Error> {
+    public async getRoveredBlocks (request: bc.GetRoveredBlocksRequest): Promise<bc.GetRoveredBlocksResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetRoveredBlocks, request.toArray())
         return result as bc.GetRoveredBlocksResponse.AsObject
     }
 
-    public async getLatestRoveredBlock(): Promise<bc.GetRoveredBlocksResponse.AsObject|Error> {
+    public async getLatestRoveredBlock (): Promise<bc.GetRoveredBlocksResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetLatestRoveredBlocks, [])
         return result as bc.GetRoveredBlocksResponse.AsObject
     }
 
-    public async getBlockHash(request: bc.GetBlockHashRequest): Promise<core.BcBlock.AsObject|Error> {
+    public async getBlockHash (request: bc.GetBlockHashRequest): Promise<core.BcBlock.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBlockHash, request.toArray())
         return result as core.BcBlock.AsObject
     }
 
-    public async getBlockHeight(request: bc.GetBlockHeightRequest): Promise<core.BcBlock.AsObject|Error> {
+    public async getBlockHeight (request: bc.GetBlockHeightRequest): Promise<core.BcBlock.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBlockHeight, request.toArray())
         return result as core.BcBlock.AsObject
     }
 
-    public async getBlocks(request: bc.GetBlocksRequest): Promise<bc.GetBlocksResponse.AsObject|Error> {
+    public async getBlocks (request: bc.GetBlocksRequest): Promise<bc.GetBlocksResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBlocks, request.toArray())
         return result as bc.GetBlocksResponse.AsObject
     }
 
-    public async getLatestBlock(): Promise<core.BcBlock.AsObject|Error> {
+    public async getLatestBlock (): Promise<core.BcBlock.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetLatestBlock, [])
         return result as core.BcBlock.AsObject
     }
 
-    public async getTx(request: bc.GetTxRequest): Promise<core.Transaction.AsObject|Error> {
+    public async getTx (request: bc.GetTxRequest): Promise<core.Transaction.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetTx, request.toArray())
         return result as core.Transaction.AsObject
     }
 
-    public async getMarkedTx(request: bc.GetMarkedTxRequest): Promise<core.MarkedTransaction.AsObject|Error> {
+    public async getMarkedTx (request: bc.GetMarkedTxRequest): Promise<core.MarkedTransaction.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetMarkedTx, request.toArray())
         return result as core.MarkedTransaction.AsObject
     }
@@ -221,27 +176,27 @@ export default class RpcClient {
         return result as bc.RpcTransactionResponse.AsObject
     }
 
-    public async getBalance(request: bc.GetBalanceRequest): Promise<bc.GetBalanceResponse.AsObject|Error> {
+    public async getBalance (request: bc.GetBalanceRequest): Promise<bc.GetBalanceResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBalance, request.toArray())
         return result as bc.GetBalanceResponse.AsObject
     }
 
-    public async getWallet(request: bc.GetBalanceRequest): Promise<core.WalletData.AsObject> {
+    public async getWallet (request: bc.GetBalanceRequest): Promise<core.WalletData.AsObject> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetWallet, request.toArray())
         return result as core.WalletData.AsObject
     }
 
-    public async getSpendableCollateral(request: bc.GetSpendableCollateralRequest): Promise<bc.GetSpendableCollateralResponse.AsObject|Error> {
+    public async getSpendableCollateral (request: bc.GetSpendableCollateralRequest): Promise<bc.GetSpendableCollateralResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetSpendableCollateral, request.toArray())
         return result as bc.GetSpendableCollateralResponse.AsObject
     }
 
-    public async unlockCollateral(request: bc.UnlockCollateralRequest): Promise<bc.RpcTransactionResponse.AsObject|Error> {
+    public async unlockCollateral (request: bc.UnlockCollateralRequest): Promise<bc.RpcTransactionResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.UnlockCollateral, request.toArray())
         return result as bc.RpcTransactionResponse.AsObject
     }
 
-    public async getUnlockTakerTxParams(request: bc.GetUnlockTakerTxParamsRequest): Promise<bc.GetUnlockTakerTxParamsResponse.AsObject> {
+    public async getUnlockTakerTxParams (request: bc.GetUnlockTakerTxParamsRequest): Promise<bc.GetUnlockTakerTxParamsResponse.AsObject> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetUnlockTakerTxParams, request.toArray())
         return result as bc.GetUnlockTakerTxParamsResponse.AsObject
     }
@@ -271,28 +226,74 @@ export default class RpcClient {
         return result as bc.FeeResponse.AsObject
     }
 
-    public async getOpenOrders(): Promise<bc.GetOpenOrdersResponse.AsObject> {
+    public async getOpenOrders (): Promise<bc.GetOpenOrdersResponse.AsObject> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetOpenOrders, [])
         return result as bc.GetOpenOrdersResponse.AsObject
     }
 
-    public async sendTx(request: core.Transaction): Promise<bc.RpcTransactionResponse.AsObject|Error> {
+    public async sendTx (request: core.Transaction): Promise<bc.RpcTransactionResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.SendTx, Buffer.from(request.serializeBinary()))
         return result as bc.RpcTransactionResponse.AsObject
     }
 
-    public async getMatchedOrders(request: bc.GetMatchedOrdersRequest): Promise<bc.GetMatchedOrdersResponse.AsObject> {
+    public async getMatchedOrders (request: bc.GetMatchedOrdersRequest): Promise<bc.GetMatchedOrdersResponse.AsObject> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetMatchedOrders, Buffer.from(request.serializeBinary()))
         return result as bc.GetMatchedOrdersResponse.AsObject
     }
 
-    public async getBlake2Bl(request: bc.GetBlake2blRequest): Promise<bc.GetBlake2blResponse.AsObject|Error> {
+    public async getBlake2Bl (request: bc.GetBlake2blRequest): Promise<bc.GetBlake2blResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBlake2bl, request.toArray())
         return result as bc.GetBlake2blResponse.AsObject
     }
 
-    public async getBcAddressViaVanity(request: bc.VanityConvertRequest): Promise<bc.VanityConvertResponse.AsObject|Error> {
+    public async getBcAddressViaVanity (request: bc.VanityConvertRequest): Promise<bc.VanityConvertResponse.AsObject|Error> {
         const result = await this.makeJsonRpcRequest(BcRpcMethod.GetBcAddressViaVanity, request.toArray())
         return result as bc.VanityConvertResponse.AsObject
+    }
+
+    private async makeJsonRpcRequest (method: BcRpcMethod, rpcParams: JsonRpcParams|Buffer): Promise<BcRpcResponse|JsonRpcError<BcRpcResponse>> {
+        const id = Math.abs(Math.random() * 1e6 | 0) /* tslint:disable-line */ // this bitwise operation is intentional
+        const rpcBody: JsonRpcRequest<BcRpcMethod> = {
+            id,
+            jsonrpc: '2.0',
+            method,
+            params: rpcParams,
+        }
+
+        let res
+        try {
+            res = await fetch(`${this.rpcUrl.origin}/rpc`, {
+                body: JSON.stringify(rpcBody),
+                headers: this.defaultHeaders,
+                method: 'post',
+            })
+        } catch (e) {
+            return {
+                code: -1,
+                message: e.toString(),
+            }
+        }
+
+        if (res.status !== 200) {
+            return {
+                code: res.status,
+                message: res.statusText,
+            }
+        }
+
+        const jsonResult: JsonRpcResponse<BcRpcResponse> = await res.json()
+
+        if ('code' in jsonResult) {
+            return jsonResult
+        }
+
+        if (jsonResult.id !== id) {
+            return {
+                code: -32603,
+                message: `Ids didn't match - sent ${id}, got ${jsonResult.id}`,
+            }
+        }
+
+        return (jsonResult as JsonRpcSuccess<BcRpcResponse>).result
     }
 }
