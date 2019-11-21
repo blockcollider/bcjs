d06de39bcfdfaf085611266bf0fcdb7d062462e2
Ping Zhang <pz@blockcollider.org>
2019-11-20 21:50:07 -0800

    add create-maker

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -58,14 +58,51 @@ command.command('create-maker')
 const makerOrderParams = [
   { option: '--shiftMaker <shiftMaker>', description: 'shiftMaker' },
   { option: '--shiftTaker <shiftTaker>', description: 'shiftTaker' },
-  { option: '--address <address>', description: 'BlockCollider address' },
-  // TODO: add more
+  { option: '--depositLength <depositLength>', description: 'depositLength' },
+  { option: '--settleLength <settleLength>', description: 'settleLength' },
+  { option: '--sendsFromChain <sendsFromChain>', description: 'sendsFromChain' },
+  { option: '--receivesToChain <receivesToChain>', description: 'receivesToChain' },
+  { option: '--sendsFromAddress <sendsFromAddress>', description: 'sendsFromAddress' },
+  { option: '--receivesToAddress <receivesToAddress>', description: 'receivesToAddress' },
+  { option: '--sendsUnit <sendsUnit>', description: 'sendsUnit' },
+  { option: '--receivesUnit <receivesUnit>', description: 'receivesUnit' },
+  { option: '--bcAddress <bcAddress>', description: 'bcAddress' },
+  { option: '--bcPrivateKeyHex <bcPrivateKeyHex>', description: 'bcPrivateKeyHex' },
+  { option: '--collateralizedNrg <collateralizedNrg>', description: 'collateralizedNrg' },
+  { option: '--nrgUnit <nrgUnit>', description: 'nrgUnit' },
+  { option: '--additionalTxFee <additionalTxFee>', description: 'additionalTxFee' },
 ]
 makerOrderParams.forEach(opt => {
   command.requiredOption(opt.option, opt.description)
 })
-command.action(cmdObj => {
-    console.log('remove ', cmdObj.affas, cmdObj.address)
+command.action(async cmdObj => {
+  const {
+    bcRpcAddress, bcRpcScookie,
+    sendsFromChain, receivesToChain,
+    sendsFromAddress, receivesToAddress,
+    sendsUnit, receivesUnit,
+    bcAddress, bcPrivateKeyHex,
+    collateralizedNrg, nrgUnit, additionalTxFee,
+  } = cmdObj
+
+  let {
+    shiftMaker, shiftTaker, depositLength, settleLength,
+  } = cmdObj
+  shiftMaker = parseInt(shiftMaker, 10)
+  shiftTaker = parseInt(shiftTaker, 10)
+  depositLength = parseInt(depositLength, 10)
+  settleLength = parseInt(settleLength, 10)
+
+  const res = await onCreateMakerTx (
+    bcRpcAddress, bcRpcScookie,
+    shiftMaker, shiftTaker, depositLength, settleLength,
+    sendsFromChain, receivesToChain,
+    sendsFromAddress, receivesToAddress,
+    sendsUnit, receivesUnit,
+    bcAddress, bcPrivateKeyHex,
+    collateralizedNrg, nrgUnit, additionalTxFee,
+  )
+  console.log(JSON.stringify(res))
 })
 
 command
@@ -73,4 +110,3 @@ command
   .requiredOption('--bcRpcScookie <bcRpcScookie>', 'BlockCollider Rpc Scookie')
 
 command.parse(process.argv)
-
