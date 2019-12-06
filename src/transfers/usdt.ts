e007fcb18b59aab2cb8db22d36cf1701f80b55f7
Ping Zhang <pz@blockcollider.org>
2019-12-06 09:08:23 -0800

    fix usdt

--- src/transfers/usdt.ts
+++ src/transfers/usdt.ts
@@ -1,17 +1,24 @@
-import {web3, USDT, submitTransaction} from './web3'
+import { submitTransaction, USDT, web3 } from './web3'
 
-export const transferUSDT = async function(privateKey, from, to, amount) {
-	try {
-    let data = USDT.methods.transfer(
-			to,
-			web3.utils.toHex(Math.floor(Math.pow(10,6)*(parseFloat(amount))))
-		).encodeABI()
-    submitTransaction({to:USDT._address,from,value:web3.utils.toHex(0),data,privateKey},(err,hash)=>{
-      if(hash) return hash
-      else return err
-    });
-  }
-	catch(err){
-		return err
-	}
+export const transferUSDT = async (privateKey, from, to, amount) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = USDT.methods
+        .transfer(
+          to,
+          web3.utils.toHex(Math.floor(Math.pow(10, 6) * parseFloat(amount))),
+        )
+        .encodeABI()
+
+      submitTransaction({ to: USDT._address, from, value: web3.utils.toHex(0), data, privateKey }, (err, hash) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(hash)
+        }
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
