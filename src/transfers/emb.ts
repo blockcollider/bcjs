0381cb19465ca8c22e55077493656b2270d63c2d
Ping Zhang <pz@blockcollider.org>
2019-12-06 08:59:56 -0800

    fix emb

--- src/transfers/emb.ts
+++ src/transfers/emb.ts
@@ -1,17 +1,23 @@
-import {web3, EMB, submitTransaction} from './web3'
+import { EMB, submitTransaction, web3 } from './web3'
 
-export const transferEMB = async function(privateKey, from, to, amount) {
-	try {
-    let data = EMB.methods.transfer(
-			to,
-			web3.utils.toHex(Math.floor(Math.pow(10,8)*(parseFloat(amount))))
-		).encodeABI()
-    submitTransaction({to:EMB._address,from,value:web3.utils.toHex(0),data,privateKey},(err,hash)=>{
-      if(hash) return hash
-      else return err
-    });
-  }
-	catch(err){
-		return err
-	}
+export const transferEMB = async (privateKey, from, to, amount) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = EMB.methods
+        .transfer(
+          to,
+          web3.utils.toHex(Math.floor(Math.pow(10, 8) * parseFloat(amount))),
+        )
+        .encodeABI()
+      submitTransaction({ to: EMB._address, from, value: web3.utils.toHex(0), data, privateKey }, (err, hash) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(hash)
+        }
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
