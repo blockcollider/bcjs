67df61538b3439fe682a48fcb4bf6ce65017c41c
Ping Zhang <pz@blockcollider.org>
2019-12-06 09:05:03 -0800

    fix lsk

--- src/transfers/lsk.ts
+++ src/transfers/lsk.ts
@@ -1,22 +1,17 @@
-const lisk = require('lisk-elements');
-var cryptography = require('@liskhq/lisk-cryptography');
-const client = lisk.APIClient.createMainnetAPIClient();
+import lisk from 'lisk-elements'
+const client = lisk.APIClient.createMainnetAPIClient()
 
-export async function payLSK(privateKey, from, to, amount){
+export const payLSK = async (privateKey, from, to, amount) => {
   try {
     const transaction = lisk.transaction.transfer({
-      amount: (amount*Math.pow(10,8)).toString(),
-      recipientId: to,
+      amount: (amount * Math.pow(10, 8)).toString(),
       passphrase: privateKey,
-    });
+      recipientId: to,
+    })
 
-    client.transactions.broadcast(transaction).then((info)=>{
-      return transaction
-    }).catch((err)=>{
-      return err
-    });
-  } catch(err){
+    return await client.transactions.broadcast(transaction)
+  } catch (err) {
     return err
-  };
+  }
 }
 
