83d3e9a77746a8dae355af613bd6f7a1fdf5fc6f
Adam Kloboucnik <adam@klob.cz>
2021-04-26 17:43:52 +0200

    Revert "Downgrade lisk-client to 2.x with --ignore-engines"
    This reverts commit eebb4df6937f86efd3488dcab77815e18d26561f.

--- dist/transfers/lsk.js
+++ dist/transfers/lsk.js
@@ -11,14 +11,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const lisk_client_1 = require("@liskhq/lisk-client");
 const client = lisk_client_1.APIClient.createMainnetAPIClient();
+const MAINNET_NETWORK_IDENTIFIER = '9ee11e9df416b18bf69dbd1a920442e08c6ca319e69926bc843a561782ca17ee';
 exports.payLSK = (privateKey, from, to, amount) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const transaction = lisk_client_1.transactions.transfer({
             amount: (amount * Math.pow(10, 8)).toString(),
+            networkIdentifier: MAINNET_NETWORK_IDENTIFIER,
             passphrase: privateKey,
             recipientId: to,
         });
-        yield client.transactions.broadcast(transaction);
+        return yield client.transactions.broadcast(transaction);
     }
     catch (err) {
         return err;
--- src/transfers/lsk.ts
+++ src/transfers/lsk.ts
@@ -1,15 +1,19 @@
 import { APIClient, transactions } from '@liskhq/lisk-client'
 const client = APIClient.createMainnetAPIClient()
 
+// based on https://lisk.io/documentation/lisk-sdk/protocol/mainnet.html
+const MAINNET_NETWORK_IDENTIFIER = '9ee11e9df416b18bf69dbd1a920442e08c6ca319e69926bc843a561782ca17ee'
+
 export const payLSK = async (privateKey, from, to, amount) => {
   try {
-    const transaction: any = transactions.transfer({
+    const transaction = transactions.transfer({
       amount: (amount * Math.pow(10, 8)).toString(),
+      networkIdentifier: MAINNET_NETWORK_IDENTIFIER,
       passphrase: privateKey,
       recipientId: to,
     })
 
-    await client.transactions.broadcast(transaction)
+    return await client.transactions.broadcast(transaction)
   } catch (err) {
     return err
   }
