d08d0bdc2b5a2a1620a2a4599f20f6416455e6d4
Adam Kloboucnik <adam@klob.cz>
2021-04-27 10:23:39 +0200

    Use lisk v2 in transfer/lsk module (remove networkIdentifier)

--- src/transfers/lsk.ts
+++ src/transfers/lsk.ts
@@ -1,14 +1,10 @@
 import { APIClient, transactions } from '@liskhq/lisk-client'
 const client = APIClient.createMainnetAPIClient()
 
-// based on https://lisk.io/documentation/lisk-sdk/protocol/mainnet.html
-const MAINNET_NETWORK_IDENTIFIER = '9ee11e9df416b18bf69dbd1a920442e08c6ca319e69926bc843a561782ca17ee'
-
 export const payLSK = async (privateKey, from, to, amount) => {
   try {
     const transaction = transactions.transfer({
       amount: (amount * Math.pow(10, 8)).toString(),
-      networkIdentifier: MAINNET_NETWORK_IDENTIFIER,
       passphrase: privateKey,
       recipientId: to,
     })
