5e03e611804813e8c7862ac54786ba600b876a29
Arjun Raj Jain <aj@blockcollider.org>
2021-06-03 12:53:09 -0400

    web3

--- dist/transfers/web3.d.ts
+++ dist/transfers/web3.d.ts
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 export declare const mainnetUrl = "https://mainnet.infura.io/v3/ca4c368803c347699a5d989cd367c0a6";
 export declare const web3: Web3;
-export declare const EMB_ADDRESS = "0xbfCdE98b92722f9BC33a5AB081397CD2D5409748";
+export declare const EMB_ADDRESS = "0xdb0acc14396d108b3c5574483acb817855c9dc8d";
 export declare const USDT_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 export declare const DAI_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
 export declare const XAUT_ADDRESS = "0x4922a015c4407F87432B179bb209e125432E4a2A";
