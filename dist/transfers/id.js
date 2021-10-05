2234a747278a8549b6ac88939e8f5f01ec57f7b9
Arjun Raj Jain <aj@blockcollider.org>
2021-10-05 17:58:33 -0400

    remove unusable infura id

--- dist/transfers/id.js
+++ dist/transfers/id.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ids = [
-    '099fc58e0de9451d80b18d7c74caa7c1',
     '6414700cfefa472eac32de35766a31c8',
 ];
 //# sourceMappingURL=id.js.map
